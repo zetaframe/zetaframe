d2155f539f5906fc5f2dd72bab4dc8f0590f5c01
wozeparrot <wozeparrot@gmail.com>
2020-04-21 18:01:08 -0400

    [math] expose matrix

--- math/src/lib.zig
+++ math/src/lib.zig
@@ -1,5 +1,11 @@
-//Vectors
+//Vector
 const vec = @import("vec.zig");
 pub const Vec2 = vec.Vec2;
 pub const Vec3 = vec.Vec3;
 pub const Vec4 = vec.Vec4;
+
+//Matrix
+const mat = @import("mat.zig");
+pub const Mat22 = mat.Mat22;
+pub const Mat33 = mat.Mat33;
+pub const Mat44 = mat.Mat44;
