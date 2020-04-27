41790a5cad6eb1fe94e0a3034e26561b45bfaddb
wozeparrot <wozeparrot@gmail.com>
2020-04-27 12:16:56 -0400

    Add non render test

--- build.zig
+++ build.zig
@@ -24,4 +24,24 @@ pub fn build(b: *Builder) void {
 
     const test_step = b.step("test", "Run All tests");
     test_step.dependOn(&tests.step);
+
+    const tests_no_render = b.addTest("test.zig");
+    tests_no_render.setBuildMode(mode);
+
+    tests_no_render.setFilter("0");
+
+    tests_no_render.addPackage(zf.corepkg);
+    tests_no_render.addPackage(zf.mathpkg);
+    tests_no_render.addPackage(zf.renderpkg);
+
+    tests_no_render.linkSystemLibrary("c");
+    tests_no_render.linkSystemLibrary("epoxy");
+    tests_no_render.linkSystemLibrary("glfw");
+    tests_no_render.linkSystemLibrary("vulkan");
+
+    tests_no_render.linkSystemLibrary("c++");
+    tests_no_render.addObjectFile("render/lib/vma/vma.o");
+
+    const test_no_render = b.step("test-no-render", "Run all but render tests");
+    test_no_render.dependOn(&tests_no_render.step);
 }
--- test.zig
+++ test.zig
@@ -2,14 +2,14 @@ const coretests = @import("core/test/test.zig");
 const mathtests = @import("math/test/test.zig");
 const rendertests = @import("render/test/test.zig");
 
-test "core" {
+test "core 0" {
     try coretests.rtest();
 }
 
-test "math" {
+test "math 0" {
     try mathtests.rtest();
 }
 
-test "render" {
+test "render 1" {
     try rendertests.rtest();
 }
