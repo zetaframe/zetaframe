13dbf9d3ab1bc54253431247d6c7d81a9991cd59
wozeparrot <wozeparrot@gmail.com>
2020-05-31 11:09:19 -0400

    [math] some math changes

--- math/src/lib.zig
+++ math/src/lib.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const trait = std.meta.trait;
 const math = std.math;
 
+// Common Functions
 pub fn deg2rad(deg: var) @TypeOf(deg) {
     comptime const Type = @TypeOf(deg);
     if (comptime trait.is(.Float)(Type) or comptime trait.is(.ComptimeFloat)(Type)) {
@@ -20,14 +21,24 @@ pub fn rad2deg(rad: var) @TypeOf(rad) {
     }
 }
 
-//Vector
+// Vector
 const vec = @import("vec.zig");
 pub const Vec2 = vec.Vec2;
 pub const Vec3 = vec.Vec3;
 pub const Vec4 = vec.Vec4;
+
+// Common Types
+pub const Vec2f = Vec2(f32);
+pub const Vec3f = Vec3(f32);
+pub const Vec4f = Vec4(f32);
 
 //Matrix
 const mat = @import("mat.zig");
 pub const Mat22 = mat.Mat22;
 pub const Mat33 = mat.Mat33;
 pub const Mat44 = mat.Mat44;
+
+// Common Types
+pub const Mat22f = Mat22(f32);
+pub const Mat33f = Mat33(f32);
+pub const Mat44f = Mat44(f32);
