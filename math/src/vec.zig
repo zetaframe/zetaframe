599e8451f60575a3f0d698c3c856bf3178522542
wozeparrot <wozeparrot@gmail.com>
2020-06-27 15:34:25 -0400

    [math] comments

--- math/src/vec.zig
+++ math/src/vec.zig
@@ -5,6 +5,7 @@ const assert = std.debug.assert;
 
 fn VecMixin(comptime Self: type, comptime T: type) type {
     return struct {
+        /// Clones the vector
         pub fn clone(self: *Self) Self {
             var result = Self.Zero;
             inline for (@typeInfo(Self).Struct.fields) |field| {
@@ -13,6 +14,7 @@ fn VecMixin(comptime Self: type, comptime T: type) type {
             return result;
         }
 
+        /// Negates the vector
         pub fn neg(self: Self) T {
             var result = Self.Zero;
             inline for (@typeInfo(Self).Struct.fields) |field| {
@@ -21,16 +23,19 @@ fn VecMixin(comptime Self: type, comptime T: type) type {
             return result;
         }
 
+        /// Gets the value at index of a vector
         pub fn getIndex(self: *Self, index: usize) T {
             assert(index < 4);
             return @field(self.*, @typeInfo(Self).Struct.fields[index].name);
         }
 
+        /// Sets the value at index of a vector
         pub fn setIndex(self: *Self, comptime index: usize, value: T) void {
             assert(index < 4);
             @field(self.*, @typeInfo(Self).Struct.fields[index].name) = value;
         }
 
+        /// Add two vectors
         pub fn add(self: Self, other: Self) Self {
             var result = Self.Zero;
             inline for (@typeInfo(Self).Struct.fields) |field| {
@@ -39,6 +44,7 @@ fn VecMixin(comptime Self: type, comptime T: type) type {
             return result;
         }
 
+        /// Sub two vectors
         pub fn sub(self: Self, other: Self) Self {
             var result = Self.Zero;
             inline for (@typeInfo(Self).Struct.fields) |field| {
@@ -47,6 +53,7 @@ fn VecMixin(comptime Self: type, comptime T: type) type {
             return result;
         }
 
+        /// Mul two vectors
         pub fn mul(self: Self, other: Self) Self {
             var result = Self.Zero;
             inline for (@typeInfo(Self).Struct.fields) |field| {
@@ -55,6 +62,7 @@ fn VecMixin(comptime Self: type, comptime T: type) type {
             return result;
         }
 
+        /// Mul a vector by a scalar
         pub fn mulScalar(self: Self, other: T) Self {
             var result = Self.Zero;
             inline for (@typeInfo(Self).Struct.fields) |field| {
@@ -63,6 +71,7 @@ fn VecMixin(comptime Self: type, comptime T: type) type {
             return result;
         }
 
+        /// Div two vectors
         pub fn div(self: Self, other: Self) Self {
             var result = Self.Zero;
             inline for (@typeInfo(Self).Struct.fields) |field| {
@@ -71,6 +80,7 @@ fn VecMixin(comptime Self: type, comptime T: type) type {
             return result;
         }
 
+        /// Div a vector by a scalar
         pub fn divScalar(self: Self, other: T) Self {
             var result = Self.Zero;
             inline for (@typeInfo(Self).Struct.fields) |field| {
@@ -79,10 +89,13 @@ fn VecMixin(comptime Self: type, comptime T: type) type {
             return result;
         }
 
+        /// Get the magnitude of a vector
         pub fn mag(self: Self) T {
             return math.sqrt(self.magSqr());
         }
 
+        /// Gets the magnitude squared of a vector
+        /// Faster than mag()
         pub fn magSqr(self: Self) T {
             var result: T = 0;
             inline for (@typeInfo(Self).Struct.fields) |field| {
@@ -91,6 +104,7 @@ fn VecMixin(comptime Self: type, comptime T: type) type {
             return result;
         }
 
+        /// Dot product of two vectors
         pub fn dot(self: Self, other: Self) T {
             var result: T = 0;
             inline for (@typeInfo(Self).Struct.fields) |field| {
@@ -99,6 +113,7 @@ fn VecMixin(comptime Self: type, comptime T: type) type {
             return result;
         }
 
+        /// Returns a vector with the smallest components of two vectors
         pub fn min(self: Self, other: Self) Self {
             var result = Self.Zero;
             inline for (@typeInfo(Self).Struct.fields) |field| {
@@ -106,6 +121,7 @@ fn VecMixin(comptime Self: type, comptime T: type) type {
             }
         }
 
+        /// Returns a vector with the largest components of two vectors
         pub fn max(self: Self, other: Self) Self {
             var result = Self.Zero;
             inline for (@typeInfo(Self).Struct.fields) |field| {
@@ -113,10 +129,13 @@ fn VecMixin(comptime Self: type, comptime T: type) type {
             }
         }
 
-        pub fn dist(self: Self) T {
-            return math.sqrt(self.distSqr());
+        /// Returns the distance between two vectors
+        pub fn dist(self: Self, other: Self) T {
+            return math.sqrt(self.distSqr(other));
         }
 
+        /// Returns the distance between two vectors squared
+        /// Faster than dist()
         pub fn distSqr(self: Self, other: Self) T {
             var result: T = 0;
             inline for (@typeInfo(Self).Struct.fields) |field| {
